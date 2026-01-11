3d94c087c1d16e8290356a2979b9ac1118f1fd80
ashish mishra <ashishmishra@zapbuild.com>
2026-01-11 15:25:56 +0530

    railsway app changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,9 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  # Allow Railway hostname
+  config.hosts << "supergrow-development.up.railway.app"
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
 
